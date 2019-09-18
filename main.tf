b210c25858b119b892748a036c17569b08ba0e84
Mark Garringer <mark@thehubmedia.com>
2019-09-18 12:55:20 -0400

    fix another error

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "template_file" "user_data" {
   template = "${file("${path.module}/user_data.sh")}"
 
-  vars {
+  vars = {
     aws_region  = "${var.region}"
     bucket_name = "${var.bucket_name}"
   }
